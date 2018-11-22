012acbea10004a46a86e69280b08dee8df10924a
JiYeonKim23 <gyp04277@gmail.com>
2018-11-22 17:58:06 +0900

    interface

---
+++ SoftwareEngineering/src/Manage_Everything.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+class contact {
+		
+}
+
+class todoList{
+	
+}
+
+class Appointment{
+	
+}
+
+public class Manage_Everything {
+	//==============================contact=====================================
+	public static int contactCreate() {
+		//성공 시 1 반환
+		//실패 시 0 반환
+		return 0;
+	}
+	public static int contactDelete() {
+		//성공 시 1 반환
+		//실패 시 0 반환
+		return 0;
+	}
+	public static void contactView() {
+		
+	}
+	public static int contactUpdate() {
+		//성공 시 1 반환
+		//실패 시 0 반환
+		return 0;
+	}
+	
+	
+	//==============================todoList=====================================
+	public static int todoCreate() {
+		//성공 시 1 반환
+		//실패 시 0 반환
+		return 0;
+	}
+	public static int todoDelete() {
+		//성공 시 1 반환
+		//실패 시 0 반환
+		return 0;
+	}
+	public static void todoView() {
+		
+	}
+	public static int todoUpdate() {
+		//성공 시 1 반환
+		//실패 시 0 반환
+		return 0;
+	}
+	//==============================Appointment=====================================
+	public static int appoCreate() {
+		//성공 시 1 반환
+		//실패 시 0 반환
+		return 0;
+	}
+	public static int appoDelete() {
+		//성공 시 1 반환
+		//실패 시 0 반환
+		return 0;
+	}
+	public static void appoView() {
+
+	}
+	public static int appoUpdate() {
+		//성공 시 1 반환
+		//실패 시 0 반환
+		return 0;
+	}
+	
+	//==============================main=====================================
+	public static void main_contact() {
+
+	}
+	public static void main_todolist() {
+
+	}
+	public static void main_appointment() {
+
+	}
+
+	//===================================================================
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
