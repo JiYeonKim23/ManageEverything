19912f5cdc44726a74561a9685fe92f9149a2fae
JiYeonKim23 <gyp04277@gmail.com>
2018-11-24 01:45:13 +0900

    basic function complete ver1

--- SoftwareEngineering/src/Manage_Everything.java
+++ SoftwareEngineering/src/Manage_Everything.java
@@ -27,7 +27,7 @@ class Appointment{
 
 public class Manage_Everything {
 	static Scanner scanner = new Scanner(System.in);
-	static HashMap<String,TodoList> todoList = new HashMap<String,TodoList>();
+	static HashMap<String,TodoList> HashMapTodoList = new HashMap<String,TodoList>();
 	//==============================contact=====================================
 	public static int contactCreate() {
 		//성공 시 1 반환
@@ -50,11 +50,23 @@ public class Manage_Everything {
 	
 	
 	//==============================todoList=====================================
-	public static int todoCreate() { //성공 시 1 반환 , 실패 시 0 반환
+	private static String todoSearch() {	//검색 >>delete,update
+		System.out.print("제목 입력:");
+		String name = scanner.nextLine();
+		if (HashMapTodoList.get(name)==null) { //해당 이름이 없을 경우
+			System.out.println("해당하는 item이 없습니다.");
+			return "0";
+		}
+		else {
+			return name;
+		}
+	}
+	
+	public static int todoCreate() {	 //성공 시 1 반환 , 실패 시 0 반환
 		TodoList todoItem = new TodoList();
 		System.out.print("제목:");
 		String name = scanner.nextLine();
-		if (todoList.get(name)==null)	// 중복되지 않는다면
+		if (HashMapTodoList.get(name)==null)	// 중복되지 않는다면
 			todoItem.setTitle(name);
 		else {
 			System.out.println("중복됩니다 다시 입력하십시오.");
@@ -66,21 +78,66 @@ public class Manage_Everything {
 		todoItem.setDue(scanner.nextLine());
 		System.out.print("상세 설명:");
 		todoItem.setDescription(scanner.nextLine());
-		todoList.put(todoItem.getTitle(), todoItem);
+		HashMapTodoList.put(todoItem.getTitle(), todoItem);
 		return 1;
 	}
-	public static int todoDelete() {
-		//성공 시 1 반환
-		//실패 시 0 반환
-		return 0;
+	
+	public static int todoDelete() {	//성공 시 1 반환, 실패 시 0 반환
+		String title = todoSearch();
+		if ( title=="0")//없다면
+			return 0;
+		else {
+			HashMapTodoList.remove(title);
+			return 1;
+		}
 	}
 	public static void todoView() {
-		
+		Set<String> keys = HashMapTodoList.keySet();
+		Iterator<String> it = keys.iterator();
+		while(it.hasNext()) {
+			String name = it.next();
+			TodoList todoItem = new TodoList();
+			todoItem = HashMapTodoList.get(name);
+			System.out.println("[제목:"+name+", 생성 날짜:"+todoItem.getCreate_date()+", 마감 날짜:"+todoItem.getDue()+",상세 설명:"+todoItem.getDescription()+"]");
+		}
 	}
-	public static int todoUpdate() {
-		//성공 시 1 반환
-		//실패 시 0 반환
-		return 0;
+
+	public static int todoUpdate() { // 성공 시 1 반환, 실패 시 0 반환
+		String title = todoSearch();
+		if (title == "0")// 없다면
+			return 0;
+		// 수정하고 싶은 것을 입력받은 후 결정
+		TodoList todoItem = HashMapTodoList.get(title);
+		System.out.println("1. Title, 2. Create date, 3. Due 4. Description");
+		System.out.print("수정하고 싶은 번호를 입력하시오 >> ");
+		int num = scanner.nextInt();
+		if (num == 1) {
+			System.out.println("Title>");
+			String updated_title = scanner.next();
+			todoItem.setTitle(updated_title);
+			HashMapTodoList.remove(title);
+			HashMapTodoList.put(updated_title, todoItem);
+		} else if (num == 2) {
+			System.out.println("생성날짜>");
+			String createDate = scanner.next();
+			todoItem.setCreate_date(createDate);
+			HashMapTodoList.remove(title);
+			HashMapTodoList.put(title,todoItem);
+		} else if (num == 3) {
+			System.out.println("마감날짜>");
+			String updated_due = scanner.next();
+			todoItem.setDue(updated_due);
+			HashMapTodoList.remove(title);
+			HashMapTodoList.put(title,todoItem);
+			
+		} else if (num == 4) {
+			System.out.println("상세설명>");
+			String updated_description = scanner.next();
+			todoItem.setDescription(updated_description);
+			HashMapTodoList.remove(title);
+			HashMapTodoList.put(title,todoItem);
+		}
+		return 1;
 	}
 	//==============================Appointment=====================================
 	public static int appoCreate() {
@@ -109,6 +166,8 @@ public class Manage_Everything {
 
 	public static void main_todolist() {
 		while (true) {
+			System.out.println("======================================================================");
+			System.out.println("======================================================================");
 			System.out.print("1.Create 2.View 3.Update 4.Delete 5.Return to main\n메뉴입력>>");
 			int num = scanner.nextInt();
 			if (num >= 1 && num <= 5)
@@ -137,7 +196,7 @@ public class Manage_Everything {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		while(true) {
-			int num=scanner.nextInt();
+			int num=2;//int num=scanner.nextInt();
 			if ( num==1)
 				main_contact();
 			else if ( num==2 )
