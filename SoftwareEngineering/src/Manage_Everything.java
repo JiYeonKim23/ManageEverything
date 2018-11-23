8437ff90658851908323466bbd78b509c4db322e
JiYeonKim23 <gyp04277@gmail.com>
2018-11-23 23:17:52 +0900

    create todoCreate()

--- SoftwareEngineering/src/Manage_Everything.java
+++ SoftwareEngineering/src/Manage_Everything.java
@@ -5,7 +5,7 @@ class contact {
 		
 }
 
-class todoList{
+class TodoList{
 	private String title;
 	private String create_date;
 	private String due;
@@ -27,6 +27,7 @@ class Appointment{
 
 public class Manage_Everything {
 	static Scanner scanner = new Scanner(System.in);
+	static HashMap<String,TodoList> todoList = new HashMap<String,TodoList>();
 	//==============================contact=====================================
 	public static int contactCreate() {
 		//성공 시 1 반환
@@ -49,10 +50,24 @@ public class Manage_Everything {
 	
 	
 	//==============================todoList=====================================
-	public static int todoCreate() {
-		//성공 시 1 반환
-		//실패 시 0 반환
-		return 0;
+	public static int todoCreate() { //성공 시 1 반환 , 실패 시 0 반환
+		TodoList todoItem = new TodoList();
+		System.out.print("제목:");
+		String name = scanner.nextLine();
+		if (todoList.get(name)==null)	// 중복되지 않는다면
+			todoItem.setTitle(name);
+		else {
+			System.out.println("중복됩니다 다시 입력하십시오.");
+			return 0;
+		}
+		System.out.print("생성 날짜:");
+		todoItem.setCreate_date(scanner.nextLine());
+		System.out.print("마감 날짜:");
+		todoItem.setDue(scanner.nextLine());
+		System.out.print("상세 설명:");
+		todoItem.setDescription(scanner.nextLine());
+		todoList.put(todoItem.getTitle(), todoItem);
+		return 1;
 	}
 	public static int todoDelete() {
 		//성공 시 1 반환
@@ -91,8 +106,28 @@ public class Manage_Everything {
 	public static void main_contact() {
 
 	}
-	public static void main_todolist() {
 
+	public static void main_todolist() {
+		while (true) {
+			System.out.print("1.Create 2.View 3.Update 4.Delete 5.Return to main\n메뉴입력>>");
+			int num = scanner.nextInt();
+			if (num >= 1 && num <= 5)
+				scanner.nextLine();
+			// file_open();
+			if (num == 1) { // 생성
+				todoCreate();
+			} else if (num == 2) { // 보기
+				todoView();
+			} else if (num == 3) { // 수정
+				todoUpdate();
+			} else if (num == 4) { // 삭제
+				todoDelete();
+			} else if (num == 5) { // 종료
+				// file_store();
+				break;
+			}
+			// file_store();
+		}
 	}
 	public static void main_appointment() {
 
