628a22ae413c8d9fec8a3653439bf3ba99776e3a
zzlhye0307 <zzlhye0307@naver.com>
2018-11-24 21:23:21 +0900

    made a change

--- SoftwareEngineering/src/Manage_Everything.java
+++ SoftwareEngineering/src/Manage_Everything.java
@@ -2,6 +2,16 @@ import java.io.*;
 import java.util.*;
 
 class Contact {
+	private String name;
+	private String number;
+	private String email;
+	
+	public String getName() {return name;}
+	public void setName() {this.name = name;}
+	public String getNumber() {return number;}
+	public void setNumber() {this.name = number;}
+	public String getEmail() {return email;}
+	public void setEmail() {this.name = email;}
 }
 
 class todoList{
@@ -12,25 +22,101 @@ class Appointment{
 }
 
 public class Manage_Everything {
+	static Scanner scanner = new Scanner(System.in);
+	static HashMap<String, Contact> contactList = new
+			Hashmap<String, Contact>();
 	//==============================contact=====================================
 	public static int contactCreate() {
 		//성공 시 1 반환
 		//실패 시 0 반환
-		return 0;
+		Contact contact = new contact();
+		System.out.print("이름: ");
+		String name = scanner.nextLine();
+		if (contactList.get(name)==null) {
+			contact.setName(name);
+		}
+		else {
+			System.out.println("중복되는 이름입니다. 다시 입력하십시오.");
+			return 0;
+		}
+		
+		System.out.print("전화번호: ");
+		contact.setNumber(scanner.nextLine());
+		System.out.print("E-mail: ");
+		contact.setEmail(scanner.nextLine());
+		contactList.put(name, Contact);
+		
+		return 1;
 	}
 	public static int contactDelete() {
 		//성공 시 1 반환
 		//실패 시 0 반환
-		return 0;
+		System.out.print("이름 입력: ");
+		String contactName = scanner.nextLine();
+		if (contactList.get(name)==null) {
+			System.out.println("That contact doesn't exist");
+			return 0;
+		}
+		else {
+			Contact contact = new Contact();
+			contact = contactList.get(name);
+			contactList.remove(name);
+			System.out.println("delete success");
+		}
+		return 1;
 	}
 	public static void contactView() {
+		Set<String> keys = contactList.keySet();
+		Iterator<String> it = keys.iterator();
+		while (it.hasNext()) {
+			String name = it.next();
+			Contact contact = new Contact();
+			contact = contactList.get(name);
+			System.out.println("[이름: " + name + 
+					" 전화번호: " + contact.getNumber() +
+					" Email: " + contact.getEmail() + "]");
+		}
 		
 	}
 	public static int contactUpdate() {
 		//성공 시 1 반환
 		//실패 시 0 반환
+		System.out.print("이름: ");
+		String name = scanner.nextLine();
+		if (contactList.get(name)==null) {
+			System.out.println("That contact doesn't exist");
+			return 0;
+		}
+		else {
+			Contact contact = new Contact();
+			contact = contactList.get(name);
+			System.out.println("1. Name, 2.Phone number, 3. E-mail");
+			int num = scanner.nextInt();
+			System.out.print("Enter what you want to update": );
+			if (num == 1) {
+				System.out.print("Name>");
+				String updated_name = scanner.next();
+				contact.setName(updated_name);
+				contactList.remove(name);
+				contactList.put(updated_name, contact);
+			}
+			else if (num == 2) {
+				System.out.print("Phone number>");
+				String updated_number = scanner.next();
+				contact.setNumber(updated_number);
+				contactList.remove(number);
+				contactList.put(updated_number, contact);
+			}
+			else if (num == 3) {System.out.print("E-mail>");
+				System.out.print("E-mail>");
+				String updated_email = scanner.next();
+				contact.setEmail(updated_email);
+				contactList.remove(email);
+				contactList.put(updated_email, contact);
+			}
+		}
 		
-		return 0;
+		return 1;
 	}
 	
 	
@@ -75,7 +161,30 @@ public class Manage_Everything {
 	
 	//==============================main=====================================
 	public static void main_contact() {
+		Scanner scanner = new Scanner(System.in);
+		int numOfContactMenu;
 		
+		file_open();
+		
+		System.out.print("\n1. Create, 2. View, 3. Update, 4. Delete, 5. Return to main");
+		numOfContactMenu = scanner.nextInt();
+		
+		while (numOfContactMenu != 5) {
+			if (numOfContactMenu == 1)
+				contactCreate();
+			else if (numOfContactMenu == 2)
+				contactView();
+			else if (numOfContactMenu == 3)
+				contactUpdate();
+			else if (numOfContactMenu == 4)
+				contactDelete();
+		
+			System.out.print("\n1. Create, 2. View, 3. Update, 4. Delete, 5. Return to main");
+			numOfContactMenu = scanner.nextInt();
+		}
+		
+		file_store();
+		scanner.close();
 	}
 	public static void main_todolist() {
 
