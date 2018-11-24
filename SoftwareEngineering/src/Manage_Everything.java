aa7c1e1ab8e362a221a77c41ecc04da06afeb7d3
zzlhye0307 <zzlhye0307@naver.com>
2018-11-24 16:43:48 +0900

    made a change

--- SoftwareEngineering/src/Manage_Everything.java
+++ SoftwareEngineering/src/Manage_Everything.java
@@ -1,8 +1,17 @@
 import java.io.*;
 import java.util.*;
 
-class contact {
-		
+class Contact {
+	private String name;
+	private String number;
+	private String email;
+	
+	public String getName() {return name;}
+	public void setName() {this.name = name;}
+	public String getNumber() {return number;}
+	public void setNumber() {this.name = number;}
+	public String getEmail() {return email;}
+	public void setEmail() {this.name = email;}
 }
 
 class todoList{
@@ -14,23 +23,87 @@ class Appointment{
 
 public class Manage_Everything {
 	static Scanner scanner = new Scanner(System.in);
+	static Hashmap<String, Contact> contactList = new
+			Hashmap<String, Contact>();
+	static FileWriter fout = null;
+	FileReader fin = null;
+	static String contactAddress = "C:\\Temp\\Contact.txt"
 	//==============================contact=====================================
 	public static int contactCreate() {
 		//성공 시 1 반환
 		//실패 시 0 반환
+		Contact contact = new contact();
+		System.out.print("이름: ");
+		String name = scanner.nextLine();
+		if (contactList.get(name)==null) {
+			contact.setName(name);
+		}
+		else {
+			System.out.println("중복되는 이름입니다.");
+			return;
+		}
+		
+		System.out.print("전화번호: ");
+		contact.setNumber(scanner.nextLine());
+		System.out.print("Email: ");
+		contact.setEmail(scanner.nextLine());
+		contactList.put(name, Contact);
+		
 		return 0;
 	}
 	public static int contactDelete() {
 		//성공 시 1 반환
 		//실패 시 0 반환
+		System.out.print("이름 입력: ");
+		String contactName = scanner.nextLine();
+		if (contactList.get(name)==null) 
+			System.out.println("That contact doesn't exist");
+		else {
+			Contact contact = new Contact();
+			contact = contactList.get(name);
+			contactList.remove(name);
+			System.out.println("delete success");
+		}
 		return 0;
 	}
 	public static void contactView() {
+		Set<String> keys = contactList.keySet();
+		Iterator<String> it = keys.iterator();
+		while (it.hasNext()) {
+			String name = it.next();
+			Contact contact = new Contact();
+			contact = contactList.get(name);
+			System.out.println("[이름: " + name + 
+					" 전화번호: " + contact.getNumber() +
+					" Email: " + contact.getEmail() + "]");
+		}
 		
 	}
 	public static int contactUpdate() {
 		//성공 시 1 반환
 		//실패 시 0 반환
+		System.out.print("이름: ");
+		String name = scanner.nextLine();
+		if (contactList.get(name)==null) {
+			System.out.println("That contact doesn't exist");
+		}
+		else {
+			Contact contact = new Contact();
+			contact = contactList.get(name);
+			System.out.println("1. Name, 2.Phone number, 3. E-mail");
+			int num = scanner.nextInt();
+			System.out.print("Enter what you want to update": );
+			if (num == 1) {
+				contact.setName(scanner.nextLine());
+			}
+			else if (num == 2) {
+				contact.setNumber(scanner.nextLine());
+			}
+			else if (num == 3) {
+				contact.setEmail(scanner.nextLine());
+			}
+		}
+		
 		return 0;
 	}
 	
@@ -76,7 +149,30 @@ public class Manage_Everything {
 	
 	//==============================main=====================================
 	public static void main_contact() {
-
+		Scanner scanner = new Scanner(System.in);
+		int numOfContactMenu;
+		
+		file_open();
+		
+		System.out.print("\n1. Create, 2. View, 3. Update, 4. Delete, 5. Return to main");
+		numOfContactMenu = scanner.nextInt();
+		
+		while (numOfContactMenu != 5) {
+			if (numOfContactMenu == 1)
+				contactCreate();
+			else if (numOfContactMenu == 2)
+				contactView();
+			else if (numOfContactMenu == 3)
+				contactUpdate();
+			else if (numOfContactMenu == 4)
+				contactDelete();
+		
+			System.out.print("\n1. Create, 2. View, 3. Update, 4. Delete, 5. Return to main");
+			numOfContactMenu = scanner.nextInt();
+		}
+		
+		file_store();
+		scanner.close();
 	}
 	public static void main_todolist() {
 
@@ -99,6 +195,5 @@ public class Manage_Everything {
 			else if ( num==4 )
 				break;
 		}
-	
 	}
 }
